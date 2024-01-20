d99ccb02c1143fd287f6c88848f92e77ebc2a427
newbin4126 <dkfmal710@gmail.com>
2024-01-21 00:24:47 +0900

    portfolio

--- next-portfolio/components/projects/project-item.js
+++ next-portfolio/components/projects/project-item.js
@@ -1,14 +1,29 @@
 import Image from "next/image";
 
 export default function ProjectItem({ data }) {
-  const title = data.properties.Name.title[0].plain_text;
-  const github = data.properties.Github.url;
-  const youtube = data.properties.Youtube.url;
-  const description = data.properties.Description.rich_text[0].plain_text;
-  const imgSrc = data.cover.file?.url || data.cover.external.url;
-  const tags = data.properties.Tags.multi_select;
-  const start = data.properties.WorkPeriod.date.start;
-  const end = data.properties.WorkPeriod.date.end;
+  const title =
+    data.properties.이름 && data.properties.이름.title[0]
+      ? data.properties.이름.title[0].plain_text
+      : "";
+  const github = data.properties.Github ? data.properties.Github.url : "";
+  const description =
+    data.properties.설명 && data.properties.설명.rich_text[0]
+      ? data.properties.설명.rich_text[0].plain_text
+      : "";
+  const imgSrc = data.cover.file
+    ? data.cover.file.url
+    : data.cover.external
+    ? data.cover.external.url
+    : "";
+  const tags = data.properties.태그 ? data.properties.태그.multi_select : [];
+  const start =
+    data.properties.WorkPeriod && data.properties.WorkPeriod.date
+      ? data.properties.WorkPeriod.date.start
+      : "";
+  const end =
+    data.properties.WorkPeriod && data.properties.WorkPeriod.date
+      ? data.properties.WorkPeriod.date.end
+      : "";
 
   const calculatedPeriod = (start, end) => {
     const startDateStringArray = start.split("-");
@@ -25,13 +40,9 @@ export default function ProjectItem({ data }) {
       endDateStringArray[2]
     );
 
-    console.log(`startDate: ${startDate}`);
-    console.log(`endDate: ${endDate}`);
-
     const diffInMs = Math.abs(endDate - startDate);
     const result = diffInMs / (1000 * 60 * 60 * 24);
 
-    console.log(`기간 : ${result}`);
     return result;
   };
 
@@ -41,8 +52,8 @@ export default function ProjectItem({ data }) {
         className="rounded-t-xl"
         src={imgSrc}
         alt="cover image"
-        width="100%"
-        height="50%"
+        width={500} // 적절한 너비 값을 설정하세요.
+        height={300} // 적절한 높이 값을 설정하세요.
         layout="responsive"
         objectFit="cover"
         quality={100}
@@ -52,9 +63,9 @@ export default function ProjectItem({ data }) {
         <h1 className="text-2xl font-bold">{title}</h1>
         <h3 className="mt-4 text-xl">{description}</h3>
         <a href={github}>깃허브 바로가기</a>
-        <a href={youtube}>유튜브 시연영상 보러가기</a>
         <p className="my-1 ">
-          작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
+          작업기간 : {start} ~ {end} (
+          {start && end ? calculatedPeriod(start, end) : ""}일)
         </p>
         <div className="flex items-start mt-2">
           {tags.map((aTag) => (
